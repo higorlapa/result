3f3ae31c1f017f79349889aef49646cecbe66a1c
Jacob  Moura <jacobaraujo7@gmail.com>
2022-12-04 14:19:41 -0300

    fix: private constructor

--- lib/src/unit.dart
+++ lib/src/unit.dart
@@ -6,13 +6,12 @@ import 'package:meta/meta.dart';
 /// There is only one value of type [Unit].
 @sealed
 class Unit {
-  static const Unit _unit = Unit._instance();
-  const Unit._instance();
+  const Unit._();
 
   @override
-  String toString() => '()';
+  String toString() => 'Unit';
 }
 
 /// Used instead of `void` as a return statement for a function when
 ///  no value is to be returned.
-const unit = Unit._unit;
+const unit = Unit._();
